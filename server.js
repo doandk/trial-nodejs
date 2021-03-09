802514b2fb0db7c87a5574bb2d3bb41508541a0c
doansinaga70 <doansinaga70@gmail.com>
2021-03-09 15:07:20 +0700

    change image version and variable

--- server.js
+++ server.js
@@ -5,15 +5,14 @@ const express = require('express');
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
-
 var os = require('os');
-os.platform(); // 'darwin'
-os.release(); //'10.8.0'
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('This is Prod');
+  os.platform();
+  os.release();
 });
 
 app.listen(PORT, HOST);
